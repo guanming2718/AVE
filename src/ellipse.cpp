91c5a5f3829fffb0b235bed29b215f2f5ed38b9d
Richard <guanming.zhang@physics.ox.ac.uk>
2020-05-19 01:07:46 +0100

    fininsh intersection add tangnet

--- src/ellipse.cpp
+++ src/ellipse.cpp
@@ -29,15 +29,28 @@ std::vector<vec2 > Ellipse::intersection(Geometry& geo){
     if (x_min>x_max) std::swap(x_min,x_max);
     if (y_min>y_max) std::swap(y_min,y_max);
     std::vector<bool> added ={false}; 
-    for (unsigned i=0;i<points.size()ji++){
+    std::vec<vec2> checked_points;
+    // eliminates points that are too close
+    for (unsigned i=0;i<points.size();i++){
+        if (added[i]) continue;
+        checked_points.push_back(points[i]);
+        added[i] = true;
         for (unsigned j=i+1;j<points.size();j++){
-            if 
-
+            if (added[j]) continue;
+            # distance between two points
+            double d = (points[i] - points[j]).norm(); 
+            if (d<0.05*a) added[j] = true;
         }
     }   
-    
-
+    return checked_points;
 }
-    
-}
+vec2 Ellipse::tangent_vector(vec& p){
+    //this point p should be on this object
+    vec<double,6> c = this->eqn_coeff();
+    vec2 t;
+    // tx = 2By+Cx+E,ty=-(2Ax+Cy+D)
+    t[0] = 2.0*c[1]*p[1] + c[2]*p[0] + c[4];
+    t[1] = -(2.0*c[0]+c[2]*p[1]+c[3]);
+    return t; 
+}    
 #endif
